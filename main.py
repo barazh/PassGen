7dcdc877fce6531351ebb5bb80e30ba4eadf10b7
barazh <barashx@gmail.com>
2021-05-30 22:54:38 +1000

    Пробный комит

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from tkinter import *
 from random import *
+from tkinter import scrolledtext
 
 
 def generate_password():
     chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&*+-=?@^_'
-    print("".join(sample(chars, 6)))
+    txt.delete(1.0, END)  # Очищаем окно текста
+    txt.insert(INSERT, "".join(sample(chars, 8)))  # Вставляем в окно снгенерируемый пароль
+    #print("".join(sample(chars, 8)))
     # return "".join(sample(chars, length))
 
 
-window = Tk()
+window = Tk()  # Основное создание окна IU
 window["bg"] = '#fafafa'  # Цвет окна
 window.title("Генератор паролей")  # Загаловок окна
-window.wm_attributes("-alpha", 1)
-window.geometry("300x250")  # Устанавливаем размер окна
-window.resizable(width=False, height=False)  # Позволяет пользователю изменять размер окна
+window.wm_attributes("-alpha", 1)  # Настройка прозрачности
+window.geometry("300x400")  # Устанавливаем размер окна
+window.resizable(width=True, height=True)  # Позволяет пользователю изменять размер окна
 
-btn = Button(window, text='Сгенерировать пароль', command=generate_password)
+btn = Button(window, text='Сгенерировать пароль', command=generate_password)  # создаем кнопку
 btn.grid(column=0, row=5)
+
+txt = scrolledtext.ScrolledText(window, width=30, height=6)
+txt.grid(column=0, row=0)
 
 
 
